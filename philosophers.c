45ed8c5329279f6ead05b2b96f64aab77b82f25f
Mohamed El Hmouid <mohamed.elhmouid@edu.uiz.ac.ma>
2025-05-25 16:18:06 +0100

    parcing

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: moel-hmo <moel-hmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 14:59:41 by moel-hmo          #+#    #+#             */
-/*   Updated: 2025/05/25 15:38:38 by moel-hmo         ###   ########.fr       */
+/*   Updated: 2025/05/25 16:12:41 by moel-hmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 int main(int ac, char **av)
 {
 	int	i = 0;
-	if (ac <= 1)
+	if (ac <= 4 || ac > 6)
+	{
+		printf("Error in the Arguments");
 		return (0);
+	}
 	while (i++ < ac - 1)
 	{
 		if (parsing_arguments(av[i]) == 1)
@@ -24,7 +27,6 @@ int main(int ac, char **av)
 			printf("Error in the Arguments");
 			return (1);
 		}
-		
 	}
 	return (0);
 }
