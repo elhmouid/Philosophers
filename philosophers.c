809073b7830bdadcb379a78af989420822da1a3e
Mohamed El Hmouid <mohamed.elhmouid@edu.uiz.ac.ma>
2025-05-25 15:39:02 +0100

    fied the segfault

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: moel-hmo <moel-hmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 14:59:41 by moel-hmo          #+#    #+#             */
-/*   Updated: 2025/05/23 17:16:48 by moel-hmo         ###   ########.fr       */
+/*   Updated: 2025/05/25 15:38:38 by moel-hmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,14 @@ int main(int ac, char **av)
 	int	i = 0;
 	if (ac <= 1)
 		return (0);
-	while (i++ < ac)
-		parsing_arguments(av[i]);
+	while (i++ < ac - 1)
+	{
+		if (parsing_arguments(av[i]) == 1)
+		{
+			printf("Error in the Arguments");
+			return (1);
+		}
+		
+	}
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,6 +6,15 @@
 # include <pthread.h>
 
 
+typedef struct s_philosophers {
+    int number_of_philosophers;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int number_of_times_each_philosopher_must_eat;
+}   t_philo;
+
+
 int	parsing_arguments(char *av);
 int	ft_atoi(char *str);
 
